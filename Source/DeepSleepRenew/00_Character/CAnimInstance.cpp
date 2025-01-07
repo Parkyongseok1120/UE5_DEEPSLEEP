fbea439edf3760d043d8255940f2f4f0f3176976
Yongseok Park <ssters999@outlook.kr>
2025-01-07 20:15:39 +0900

    playerMoving bug fix

--- Source/DeepSleepRenew/00_Character/CAnimInstance.cpp
+++ Source/DeepSleepRenew/00_Character/CAnimInstance.cpp
@@ -28,18 +28,22 @@ void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 	C_Speed = PlayerCharacter->GetVelocity().Size2D();
 	FRotator rotator = PlayerCharacter->GetVelocity().ToOrientationRotator(); //현재 가는 벡터위치의 회전값을 구함.
 	FRotator rotator2 = PlayerCharacter->GetControlRotation(); // 카메라의 회전방향
-	FRotator delta = UKismetMathLibrary::NormalizedDeltaRotator(rotator, rotator2); // 두 회전값의 평균을 구함
+	FRotator DeltaRotation = UKismetMathLibrary::NormalizedDeltaRotator(rotator, rotator2); // 두 회전값의 평균을 구함
 
 	//// 이전 회전값.
-	PrevRotation = UKismetMathLibrary::RInterpTo(PrevRotation, delta, DeltaSeconds, 100);
+	PrevRotation = UKismetMathLibrary::RInterpTo(PrevRotation, DeltaRotation, DeltaSeconds, 100.0f);
 	C_Direction = PrevRotation.Yaw;
 	
-	if(C_Direction >= 170 && C_Direction <= -170) //Direction의 값이 170이상이거나 -170이하일 경우,
+	if (FMath::Abs(C_Direction) > 146.0f) // 뒤로 이동 방향을 감지
 	{
-		float NewDirection = FMath::Abs(C_Direction); //Direction을 절댓값으로 바꾸고 
-		C_Direction = FMath::Lerp(C_Direction, NewDirection, DeltaSeconds * 4.0f);
-		//Lerp 함수로 이전프레임의 Direction값과 현재 프레임의 Direction을 선형보간함.
+		if (C_Speed <= 10.0f)
+			C_Direction = FMath::Abs(0);
+		C_Direction = FMath::Abs(180.0f);//;FMath::Clamp(C_Direction, -15.0f, 155.0f); // 극단적인 값을 제한
 	}
+	// 방향값의 급격한 변화를 제한하여 부드럽게 만듦
+	static float SmoothedDirection = 0.0f;
+	SmoothedDirection = FMath::FInterpTo(SmoothedDirection, C_Direction, DeltaSeconds, 8.0f); // 보간 속도 설정
+	C_Direction = SmoothedDirection;
 	
 	bFalling = PlayerCharacter->GetCharacterMovement()->IsFalling();
 }
